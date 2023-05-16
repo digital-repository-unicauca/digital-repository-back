6bcf7d3e4a282e2e03397b9ec28710dc6178cb9b
Julian Santiago Martinez <juliansmartinez@unicauca.edu.co>
2023-05-15 21:48:26 -0500

    Added: Layers architecture base project

---
+++ digital-repository-back/src/main/java/co/unicauca/digital/repository/back/DigitalRepositoryApplication.java
@@ -0,0 +1,12 @@
+package co.unicauca.digital.repository.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalRepositoryApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalRepositoryApplication.class, args);
+	}
+
+}
