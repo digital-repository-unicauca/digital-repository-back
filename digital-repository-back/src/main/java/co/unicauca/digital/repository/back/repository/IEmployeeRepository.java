1fe3e09a7860eb1fe371c3774e2cece4223f379a
Julian Santiago Martinez <juliansmartinez@unicauca.edu.co>
2023-05-15 21:29:40 -0500

    Revert "2"
    This reverts commit 209deb2a95b47675dac610d13b99a4c351f01049.

--- digital-repository-back/src/main/java/co/unicauca/digital/repository/back/repository/IEmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package co.unicauca.digital.repository.back.repository;
-
-import co.unicauca.digital.repository.back.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IEmployeeRepository extends JpaRepository<Employee, Integer> {
-}
