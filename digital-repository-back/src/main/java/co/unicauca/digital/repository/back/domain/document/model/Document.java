5446ec193d838bad7784b73022d791f518a67406
Miguel Mosquera <miguel.mosquera@mdsis.com.co>
2023-05-27 13:11:02 -0500

    Correcciones :*

--- digital-repository-back/src/main/java/co/unicauca/digital/repository/back/domain/document/model/Document.java
+++ digital-repository-back/src/main/java/co/unicauca/digital/repository/back/domain/document/model/Document.java
@@ -5,50 +5,59 @@ import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name="document")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class Document {
+    /** Document id */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    /** Document id */
     private Integer id;
-    @Column(length = 250)
+
     /** Document name */
+    @Column(length = 250)
     private String name;
+
     /** Document description */
     private String description;
+
     /** Document Ordering */
     private Integer ordering;
+
     /** Document url */
     private Integer url;
+
     /** Document Consecutive */
     private Integer consecutive;
+
     /** Document isException */
     private boolean isException;
-    @Column(length = 250)
+
     /** Document type */
+    @Column(length = 250)
     private String type;
+
     /** Document expeditionDate */
     private LocalDateTime expeditionDate;
-    @Column(length = 250)
+
     /** Document create user */
+    @Column(length = 250)
     private String createUser;
+
     /** Document create time */
     private LocalDateTime createTime;
-    @Column(length = 250)
+
     /** Document update user */
+    @Column(length = 250)
     private String updateUser;
+
     /** Document update time */
     private LocalDateTime updateTime;
-
-    /**
-    @ManyToOne
-    @JoinColumn(name = "collectionId")
-    private Collection collection;
-    */
+//
+//    @ManyToOne
+//    @JoinColumn(name = "collectionId")
+//    private Collection collection;
 
 }
