2d084a88b9cd741b0e60f01ed2fec6c3c0e2410f
Sebastian Arenas <sebastian.arenas@hemisferiod.co>
2023-05-26 23:45:09 -0500

    Primer avanze CRUD contractualDocument

---
+++ digital-repository-back/src/main/java/co/unicauca/digital/repository/back/domain/contractualDocument/mapper/IContractualDocumentMapper.java
@@ -0,0 +1,43 @@
+package co.unicauca.digital.repository.back.domain.contractualDocument.mapper;
+
+
+import co.unicauca.digital.repository.back.domain.contractualDocument.dto.request.ContractualDocumentDtoCreateRequest;
+import co.unicauca.digital.repository.back.domain.contractualDocument.dto.response.ContractualDocumentDtoCreateResponse;
+import co.unicauca.digital.repository.back.domain.contractualDocument.dto.response.ContractualDocumentDtoFindResponse;
+import co.unicauca.digital.repository.back.domain.contractualDocument.model.ContractualDocument;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+@Mapper(componentModel = "spring")
+public interface IContractualDocumentMapper {
+    @Mappings({
+            @Mapping(target = "description", source = "description"),
+            @Mapping(target = "isRequired", source = "isRequired"),
+            @Mapping(target = "subdirectory", source = "subdirectory"),
+            @Mapping(target = "ordering", source = "ordering"),
+            @Mapping(target = "createTime", source = "createTime"),
+            @Mapping(target = "createUser", source = "createUser")
+    })
+    ContractualDocumentDtoCreateResponse toDtoCreate(final ContractualDocument contractualDocumentType);
+
+    @Mappings({
+            @Mapping(target = "description", source = "description"),
+            @Mapping(target = "isRequired", source = "isRequired"),
+            @Mapping(target = "subdirectory", source = "subdirectory"),
+            @Mapping(target = "ordering", source = "ordering"),
+            @Mapping(target = "createUser", source = "createUser")
+    })
+    ContractualDocument toEntityCreate(final ContractualDocumentDtoCreateRequest contractualDocumentTypeDtoCreateRequest);
+
+    @Mappings({
+            @Mapping(target = "id", source = "id"),
+            @Mapping(target = "description", source = "description"),
+            @Mapping(target = "isRequired", source = "isRequired"),
+            @Mapping(target = "subdirectory", source = "subdirectory"),
+            @Mapping(target = "ordering", source = "ordering"),
+            @Mapping(target = "createTime", source = "createTime"),
+            @Mapping(target = "createUser", source = "createUser")
+    })
+    ContractualDocumentDtoFindResponse toDtoFind(final ContractualDocument contractualDocument);
+}
