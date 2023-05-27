e3ccb5db9809a378ada03c74f28ec68c649e89d5
DavidCamilo <davideescobarr@gmail.com>
2023-05-27 13:21:28 -0500

    documentation

--- digital-repository-back/src/main/java/co/unicauca/digital/repository/back/domain/collection/service/ICollectionService.java
+++ digital-repository-back/src/main/java/co/unicauca/digital/repository/back/domain/collection/service/ICollectionService.java
@@ -5,40 +5,48 @@ import co.unicauca.digital.repository.back.domain.collection.dto.response.Collec
 import co.unicauca.digital.repository.back.global.response.PageableResponse;
 import co.unicauca.digital.repository.back.global.response.Response;
 
+/**
+ * Interface that allows defining the business operations to be carried out on the Collection entity.
+ */
 public interface ICollectionService {
     /**
+     * Service to create a collection
      *
-     * @param collectionDtoRequest
-     * @return
+     * @param collectionDtoRequest Object with the information to be created
+     * @return Response object for the service, containing information about the transaction outcome.
      */
     Response<CollectionDtoResponse> createCollection(final CollectionDtoRequest collectionDtoRequest);
 
     /**
+     * Service to get a collection by ID
      *
-     * @param id
-     * @return
+     * @param id ID of the collection to retrieve
+     * @return Response object for the service, containing information about the transaction outcome.
      */
     Response<CollectionDtoResponse> getByIdCollection(Integer id);
 
     /**
+     * Service to get all collections
      *
-     * @param pageNumber
-     * @param pagezise
-     * @return
+     * @param pageNumber Pagination page number
+     * @param pagezise Pagination page size
+     * @return Response object for the service, containing information about the transaction outcome.
      */
     Response<PageableResponse<Object>> getAll(int pageNumber, int pagezise);
 
     /**
+     * Service to update a collection
      *
-     * @param collectionDtoRequest
-     * @return
+     * @param collectionDtoRequest Object with the information to be updated
+     * @return Response object for the service, containing information about the transaction outcome.
      */
     Response<CollectionDtoResponse> update(final CollectionDtoRequest collectionDtoRequest);
 
     /**
+     * Service to delete a collection by ID
      *
-     * @param id
-     * @return
+     * @param id ID of the collection to delete
+     * @return Response object for the service, indicating whether the deletion was successful.
      */
     Response<Boolean> delete(Integer id);
 }
