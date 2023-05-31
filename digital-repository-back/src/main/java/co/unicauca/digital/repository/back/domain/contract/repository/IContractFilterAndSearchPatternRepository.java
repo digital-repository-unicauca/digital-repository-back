06b17aea17ed2c15fd61fdb4ed73efa25f6a7c46
Miguel Mosquera <miguel.mosquera@mdsis.com.co>
2023-05-31 09:17:36 -0500

    Correcion filter

--- digital-repository-back/src/main/java/co/unicauca/digital/repository/back/domain/contract/repository/IContractFilterAndSearchPatternRepository.java
+++ digital-repository-back/src/main/java/co/unicauca/digital/repository/back/domain/contract/repository/IContractFilterAndSearchPatternRepository.java
@@ -29,7 +29,7 @@ public interface IContractFilterAndSearchPatternRepository extends JpaRepository
                             "(:filter = 'SUPERSCRIBE-YEAR' AND YEAR(CON.signingDate) LIKE CONCAT('%', :search, '%')))",
             countQuery = "SELECT COUNT(*) " +
                     "FROM contract CON, modality MODA, contracttype CONTYPE, modalitycontracttype CONTYPEMODA, vendor VEN " +
-                    "WHERE CONTYPEMODA.modalityId = MODA.id AND CONTYPEMODA.contractTypeId = CONTYPE.id AND VEN.id = CON.vendorId AND " +
+                    "WHERE CON.modalityContractTypeId = CONTYPEMODA.id and CON.vendorId = VEN.id and CONTYPEMODA.modalityId = MODA.id AND CONTYPEMODA.contractTypeId = CONTYPE.id AND " +
                     "((:filter = 'REFERENCE' AND CON.reference LIKE CONCAT('%', :search, '%')) OR " +
                     "(:filter = 'MODALITY' AND MODA.name LIKE CONCAT('%', :search, '%')) OR " +
                     "(:filter = 'TYPE' AND CONTYPE.name LIKE CONCAT('%', :search, '%')) OR " +
